d0f140111a8b9e0a4ecbcda7f4a33ebc8b4c1f1e
Brian Polasek <brianjohnpolasek@gmail.com>
2020-02-17 21:14:15 -0600

    Basic data retrieval through panda outputs to CSV file in data folder

--- stonks.py
+++ stonks.py
@@ -3,15 +3,28 @@ import sys
 
 import os
 
+import json
+
 import yfinance as yf
+yf.pdr_override()
 
 from pandas_datareader import data as pdr
 
-tsla = yf.Ticker("TSLA")
-yf.pdr_override()
+data = pdr.get_data_yahoo("SPY", start="2017-01-01", end="2017-04-30")
 
-f = open("output.json", "a")
-f.write("tsla.major_holders")
+
+# tsla = yf.Ticker("TSLA")
+
+# data = tsla.info
+
+'''
+f = open("data/output.json", "a")
+f.write(json.dumps(str(data)))
+f.close()
+'''
+
+f = open("data/output.csv", "a")
+f.write(str(data))
 f.close()
 
 print "Done."
